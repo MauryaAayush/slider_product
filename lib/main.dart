d7a70dbb6e388d702a3dee985a7ec51154658811
MauryaAayush <maayush235@gmail.com>
2024-02-18 13:21:38 +0530

    Radhey Radhey

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,8 @@ class Slider_list extends StatefulWidget {
   State<Slider_list> createState() => _Slider_listState();
 }
 
+double amount = 500;
+
 class _Slider_listState extends State<Slider_list> {
   @override
   Widget build(BuildContext context) {
@@ -24,10 +26,12 @@ class _Slider_listState extends State<Slider_list> {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blueAccent,
+
           title: Text("Product List",style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.w500
           ),),
+          elevation: 10,shadowColor: Colors.black,
           centerTitle: true,
           actions: [
             Icon(
@@ -38,8 +42,32 @@ class _Slider_listState extends State<Slider_list> {
           ],
 
         ),
-        body: Container(
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 40, 20, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
 
+              Slider(
+                min: 100,
+                  max: 80000,
+                  value: amount,
+                  onChanged: (value) {
+
+                  },),
+              
+              Text('All Product < Rs. 30000'),
+              SizedBox(height:
+                20,),
+              Container(
+                  height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.red
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
